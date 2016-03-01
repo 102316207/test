d9953215a85ad3575ead7378567a5965e343fffc
102316207 <102316207@gms.tcu.edu.tw>
2016-03-02 01:05:16 +0800

    Create seven.java

---
+++ seven.java
@@ -0,0 +1,9 @@
+class seven{
+	public static void main(String[] args) {
+ 		System.out.println("九月九日憶山東兄弟  王維");
+		System.out.println("\t獨在异鄉為异客，");
+		System.out.println("\t每逢佳節倍思親。");
+		System.out.println("\t遙知兄弟登高處，");
+		System.out.println("\t遍插茱萸少一人。");
+	}
+}
